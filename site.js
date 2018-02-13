73ea460389b5a80319d1b77773f1d7f8423e7542
Eirizarr <eirizarr@hawk.iit.edu>
2018-02-12 23:13:28 -0600

    Added js from pp-01, this is not the final js. More like a base to reflect off and go off of.

--- site.js
+++ site.js
@@ -1,1 +1,20 @@
-
+$('#form').on('submit', function(e) {
+   var birthdate = new Date(document.getElementById("birthdate").value);
+   var today = new Date();
+   var userdate = birthdate.getFullYear();
+   var todaydate = today.getFullYear();
+   var age = todaydate - userdate;
+   var Confirmation = document.getElementById("confirmation");
+   var old = "Form was submitted successfully. See you o the Discord!";
+   var young = "Sorry, you are too young for this form to be accepted. We do not want any squeakers.";
+   e.preventDefault();
+   console.log ('This form was submitted');
+   if(age < 18) {
+     confirmation.textContent = young;
+       document.getElementById("confirmation").style.color = 'red';
+   }
+   if (age >= 18) {
+     confirmation.textContent = old;
+       document.getElementById("confirmation").style.color = 'green';
+   }
+ });
