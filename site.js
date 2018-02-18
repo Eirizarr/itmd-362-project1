7322ead23c9b6d9b115e230b3f548edf171b207d
Eirizarr <eirizarr@hawk.iit.edu>
2018-02-18 12:50:56 -0600

    Edited js to check for text fields, and check email.

--- site.js
+++ site.js
@@ -1,20 +1,31 @@
 $('#form').on('submit', function(e) {
-   var birthdate = new Date(document.getElementById("bday").value);
-   var today = new Date();
-   var userdate = birthdate.getFullYear();
-   var todaydate = today.getFullYear();
-   var age = todaydate - userdate;
+   var username = $('#name').val();
+   var useremail = $('#email').val();
+   var age = $('#age').val();
+   var checkemail = /^([a-zA-Z0-9_])+(([a-zA-Z0-9])+)+([a-zA-Z0-9]{2,4})+$/;
    var Confirmation = document.getElementById("confirmation");
-   var old = "Form was submitted successfully. See you o the Discord!";
-   var young = "Sorry, you are too young for this form to be accepted. We do not want any squeakers.";
+   var success = "Form was submitted successfully. See you o the Discord!";
+   var fill = "Please fill out the form.";
+   var sorry = "Sorry, you are too young for this form to be accepted. We do not want any squeakers.";
+   var unvalid = "Your email is not valid."
+
    e.preventDefault();
-   console.log ('This form was submitted');
-   if(age < 18) {
-     confirmation.textContent = young;
-       document.getElementById("confirmation").style.color = 'red';
-   }
-   if (age >= 18) {
-     confirmation.textContent = old;
-       document.getElementById("confirmation").style.color = 'green';
+   console.log ('This form was attemptedly submitted');
+   if(document.getElementById("#email").value !== '' && !checkEmail.test(useremail))
+    {
+        confirmation.textContent = unvalid;
+        document.getElementById("confirmation").style.color = 'red';
+      return false;
+    }
+
+   if (username === '' || useremail === '' || age === '') {
+      confirmation.textContent = fill;
+      document.getElementById("confirmation").style.color = 'red';
+    }
+    else{
+      console.log("Submission Completed", username, useremail, age);
+      confirmation.textContent = success;
+      document.getElementById("confirmation").style.color = 'green';
+    }
    }
  });
